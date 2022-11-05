caf222fa2a90373a79e61c55b546dd5f1cad95e9
z0rr01969 <cardenaslufer@gmail.com>
2022-11-05 12:38:20 -0500

    carpeta middleware

---
+++ middlewares/authVerify.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+//bearer TOKEN. 
+const autVerify = (req, res, next) => {
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'bearer') {
+        const token = re.headers.aithorization.split(' ')[1];
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+            req.payload = decoded;
+        } catch (error) {
+            return res.status(401).send('Unauthorized');
+        }
+    } else {
+        return res.status(400).send('Token is mandatory');
+    }
+    next ();
+}
+
+module.exports = {
+    autVerify,
+}
