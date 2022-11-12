28f0a4b8510c67ee012ad6bf1dfa7a59fc60e396
z0rr01969 <cardenaslufer@gmail.com>
2022-11-12 08:52:39 -0500

    install cors

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 
 require('dotenv').config();
 require('./connection/mongoconn');
